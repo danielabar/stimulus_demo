d9c11cd829d80e0fd1cb944965aed9a09e08bb24
Daniela Baron <baron.daniela@gmail.com>
2024-04-04 09:48:51 -0400

    hook up content target

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,7 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
+  static targets = ["content"]
+
   connect() {
     console.log("=== CLIPBOARD CONTROLLER CONNECTED ===")
+  }
+
+  copy() {
+    const text = this.contentTarget.innerText
+    console.log(`=== TEXT TO BE COPIED: ${text} ===`)
   }
 }
