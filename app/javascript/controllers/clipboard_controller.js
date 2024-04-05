61e1646d57a65d5892573da2a8318f72e016aa51
Daniela Baron <baron.daniela@gmail.com>
2024-04-05 09:34:48 -0400

    write to clipboard and update button text

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,14 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
-  static targets = ["content"]
-
-  connect() {
-    console.log("=== CLIPBOARD CONTROLLER CONNECTED ===")
-  }
+  static targets = ["content", "button"]
 
   copy() {
     const text = this.contentTarget.innerText
-    console.log(`=== TEXT TO BE COPIED: ${text} ===`)
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        this.buttonTarget.textContent = 'Copied';
+        setTimeout(() => {
+          this.buttonTarget.textContent = 'Copy';
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error('Failed to copy text to clipboard:', error);
+      });
   }
 }
